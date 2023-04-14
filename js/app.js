211b8c431dd4be84762c2764ee095d768ba13b87
Alejandro Jacobo <medrano182903@gmail.com>
2023-04-13 21:55:06 -0500

    feat: added json internationalization

--- js/app.js
+++ js/app.js
@@ -1,19 +1,19 @@
 const languagesElement = document.getElementById("languages");
 
-const textsToChange = document.querySelectorAll('[data-section]');
+const textsToChange = document.querySelectorAll("[data-section]");
 
 const changeLanguage = async (language) => {
-    const requestJson = await fetch(`./languages/${language}.json`);
-    const texts = await requestJson.json();
+  const requestJson = await fetch(`./languages/${language}.json`);
+  const texts = await requestJson.json();
 
-    for(const textToChange of textsToChange) {
-        const section = textToChange.dataset.section;
-        const value = textToChange.dataset.value;
+  for (const textToChange of textsToChange) {
+    const section = textToChange.dataset.section;
+    const value = textToChange.dataset.value;
 
-        textToChange.innerHTML = texts[section][value];
-    }
-}
+    textToChange.innerHTML = texts[section][value];
+  }
+};
 
 languagesElement.addEventListener("click", (e) => {
-    changeLanguage(e.target.dataset.language)
-})
+  changeLanguage(e.target.dataset.language);
+});
