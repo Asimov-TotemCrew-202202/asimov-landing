e5b479dd0ff962e5b5066674b1caa6ede8242503
Alejandro Jacobo <medrano182903@gmail.com>
2023-04-13 19:59:55 -0500

    feat: finished international configuration

--- js/app.js
+++ js/app.js
@@ -1,10 +1,17 @@
 const languagesElement = document.getElementById("languages");
 
+const textsToChange = document.querySelectorAll('[data-section]');
+
 const changeLanguage = async (language) => {
     const requestJson = await fetch(`./languages/${language}.json`);
     const texts = await requestJson.json();
 
-    console.log(texts)
+    for(const textToChange of textsToChange) {
+        const section = textToChange.dataset.section;
+        const value = textToChange.dataset.value;
+
+        textToChange.innerHTML = texts[section][value];
+    }
 }
 
 languagesElement.addEventListener("click", (e) => {
